9da7b958abd9b88d83368a160a4469ef5b8ab00e
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-01-01 23:27:25 +0600

    changed items

--- src/assets/Componets/Footer.jsx
+++ src/assets/Componets/Footer.jsx
@@ -20,9 +20,7 @@ const Footer = () => {
   </nav>
   <nav>
     <h6 className="footer-title">Legal</h6>
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
+    
   </nav>
 </footer>
         </div>
